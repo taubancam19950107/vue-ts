1da0ab9f46a0ed772deac72effc2908055bd087a
tau.ntl <tau.ntl@neo-lab.vn>
2020-10-15 18:07:13 +0700

    fix sw

---
+++ src/service-worker.js
@@ -0,0 +1,3 @@
+/* eslint-disable */
+self.__precacheManifest = [].concat(self.__precacheManifest || [])
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {})
--- vue.config.js
+++ vue.config.js
@@ -1,13 +1,33 @@
+/* eslint-disable */
+
+const CompressionWebpackPlugin = require('compression-webpack-plugin')
+
 module.exports = {
   pluginOptions: {
 
   },
+  configureWebpack: {
+    performance: {
+      hints: false
+    },
+    plugins: [
+      new CompressionWebpackPlugin({
+        algorithm: 'gzip',
+        threshold: 10240,
+        minRatio: 0.8
+      }),
+    ],
+  },
   pwa: {
-    manifestCrossorigin: 'use-credentials'
+    workboxPluginMode: 'InjectManifest',
+    workboxOptions: {
+      swSrc: './src/service-worker.js',
+      importWorkboxFrom: 'local',
+    },
   },
   devServer: {
-    host: 'web-template.ts',
-    port: 8888,
+    // host: 'web-template.ts',
+    // port: 8888,
     https: true
   }
 }
